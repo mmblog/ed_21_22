aaadf85b7b2b9115d7fdeb87222a078711034b6b
Manuel Alejandro Márquez <manuelalejandromarquezlopez@iestorredelrey.es>
2022-04-06 12:24:54 +0200

    Las cosas como son -Manuel alejandro

--- Holamundo/src/Holamundo.java
+++ Holamundo/src/Holamundo.java
@@ -3,6 +3,7 @@ public class Holamundo {
 	public void main () {
 		System.out.println("Hola mundo");
 		System.out.println("Primer cambio propio");
+		System.out.println("Esto esta editado por Manuel Alejandro Marquez lopez");
 		
 	}
 	
