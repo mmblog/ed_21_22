9e42f3c0658bfee754d10437f6e6e2bc929017a6
isabelglezb <isabelrociogonzalezbecerril@iestorredelrey.es>
2022-04-06 11:20:23 +0200

    Cambio de subida

--- Calculadora/src/Subida.java
+++ Calculadora/src/Subida.java
@@ -4,6 +4,7 @@ public class Subida {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Comprobación de subida");
+System.out.println("Mensaje para git");
 	}
 
 }
