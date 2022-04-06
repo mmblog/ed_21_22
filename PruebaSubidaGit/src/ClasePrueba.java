85eb00bf149156bab1a8631616fed0ee806ce106
AntFerUrb <antoniofernandezurbina@iestorredelrey.es>
2022-04-06 12:05:28 +0200

    Segundo commit

--- PruebaSubidaGit/src/ClasePrueba.java
+++ PruebaSubidaGit/src/ClasePrueba.java
@@ -5,6 +5,8 @@ public class ClasePrueba {
 		// TODO Auto-generated method stub
 		System.out.println("Primera prueba se subida para ver cambios en la clase");
 		System.out.println("Primer cambios en la clase");
+		System.out.println("Otro cambio");
+
 
 	}
 	
