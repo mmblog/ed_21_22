10158b8d8b03d278a8dd0f0d1a57312929bae3df
Manuel Alejandro Márquez <manuelalejandromarquezlopez@iestorredelrey.es>
2022-04-06 11:28:43 +0200

    Mi tercer comi

--- Subidamundo.v2/src/CAJA2/Frases.java
+++ Subidamundo.v2/src/CAJA2/Frases.java
@@ -5,7 +5,7 @@ public class Frases {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo cruel");
 		System.out.println("La vida no es lo que parece, es peor -Manuel alejandro márquez lópez");
-		System.out.println("jj");
+		System.out.println("Con los donuts te roban el centro -Manuel alejandro márquez lópez");
 
 	}
 
