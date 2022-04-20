73440a5d63faf3f9f597577322ad914827ed74ec
Usuario <Usuario@DESKTOP-OA178EB>
2022-04-20 12:04:22 +0200

    Cambio hecho por el delegado

--- GitHub/src/GitHub.java
+++ GitHub/src/GitHub.java
@@ -5,7 +5,7 @@ public class GitHub {
 		// TODO Auto-generated method stub
 		System.out.println("Hola, esto es una prueba");
 		System.out.println("Cambio con otro mensaje");
-		
+		System.out.println("Cambio echo por el Bola");
 		System.out.println("Cambio con los compañeros 1");
 	}
 
